70affb3725ac9be7f66c52eec2e077d72a716901
Mr.Lin <lsx0044@gmail.com>
2026-03-11 01:21:46 +0800

    Delete worker directory

--- worker/engine.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-def detect_device():
-
-    if torch.cuda.is_available():
-        return "cuda"
-
-    return "cpu"
--- worker/models/download.py
+++
@@ -1,8 +0,0 @@
-from faster_whisper import WhisperModel
-
-def download_model(model_name):
-
-    WhisperModel(
-        model_name,
-        device="cpu"
-    )
